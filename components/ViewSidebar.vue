45bae9ae9218a86c1c64fdbc53a905190be51599
jesseln <48170789+jesseln@users.noreply.github.com>
2023-09-07 20:08:06 +1000

    Sidebar -reduction

--- components/ViewSidebar.vue
+++ components/ViewSidebar.vue
@@ -3,7 +3,7 @@
   <div class="sidebar" :style="{ width: sidebarWidth }">
 
     <div class="collapse-icon" :class="{ 'rotate-180': collapsed }" @click="toggleSidebar">
-      <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" fill="none">
+      <svg xmlns="http://www.w3.org/2000/svg" :width="13*toggleIconSF" :height="14*toggleIconSF" viewBox="0 0 13 14" fill="none">
       <rect x="0.777778" y="0.777778" width="3.11111" height="12.4444" rx="1.55556" stroke="rgb(63, 63, 63)" stroke-width="1.55556" fill="none"/>
       <path d="M11.6667 1.55554L7 7.20163L11.6667 12.4444" stroke="rgb(63, 63, 63)" stroke-width="1.55556" stroke-linejoin="round" fill="none"/>
       </svg>
@@ -77,9 +77,10 @@
 
 <script setup>
 const collapsed = ref(true)
-const iconScaleFactor = ref(0.6)
+const toggleIconSF = ref(0.8)
+const iconScaleFactor = ref(0.5)
 const SIDEBAR_WIDTH = 25
-const SIDEBAR_WIDTH_COLLAPSED = 3
+const SIDEBAR_WIDTH_COLLAPSED = 2.5
 const toggleSidebar = () => (collapsed.value = !collapsed.value)
 
 const sidebarWidth = computed(
